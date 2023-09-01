6beae9f66a80c9fa190c117fd23ea9264e7e2870
rajeshvallapudasu <137849339+rajeshvallapudasu@users.noreply.github.com>
2023-09-01 07:45:54 +0530

    verifying the user answer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import turtle
 import random
 import pandas
+from tkinter import messagebox
 screen=turtle.Screen()
 screen.title("My Indian States game")
 image="rajeshindia.gif"
@@ -9,6 +10,7 @@ turtle.shape(image)
 data=pandas.read_csv("50_states.csv")
 states_list=data["state"].to_list()
 random.shuffle(states_list)
+print(states_list)
 guessed_states=[]
 for state in states_list:
     new_state = data[data["state"] == state]
@@ -20,21 +22,33 @@ for state in states_list:
     t.goto(float(new_x),float(new_y))
     t.write(new_state.no.item(),font=("ariel",10,"bold"))
 a=0
+score=0
 while a<36:
     question_state=states_list[a]
-    answer_input = int(screen.textinput(title=f"{len(guessed_states)}/36 of total states and UTs",
+    answer_input = int(screen.textinput(title="guess the  states and UTs",
                                      prompt=f"where's  the {question_state}"))
-    if answer_input==data[data["state"] == question_state].no.item():
-         guessed_states.append(question_state)
+    answer_no=data[data["state"] == question_state].no.item()
+    if answer_input==answer_no:
+        score+=1
+        guessed_states.append(question_state)
 #         state=data[data["state"]==answer_input]
-
-    elif answer_input==0:
-        missed_states=[]
-        for state in states_list:
-            if state not in guessed_states:
-                missed_states.append(state)
-        pandas.DataFrame(missed_states).to_csv("missed_states.csv")
-
+        messagebox.showinfo(title=f"answer is {answer_no}", message=f"you guessed it correct ,your score is {score}/36")
+    elif answer_input == 0:
         break
+    else:
+        messagebox.showinfo(title=f"answer is {answer_no}", message=f"wrong answer your score is {score}/36")
     a += 1
+missed_states = [state for state in states_list if state not in guessed_states]
+for state in missed_states:
+    new_state = data[data["state"] == state]
+    t = turtle.Turtle()
+    t.color("red")
+    t.penup()
+    t.goto(int(new_state.x), int(new_state.y))
+    t.hideturtle()
+    t.write(state, font=("ariel", 10, "bold"))
 
+pandas.DataFrame(missed_states).to_csv("missed_states.csv")
+messagebox.showinfo(title="quiz completed", message=f"your score is {score}/36")
+
+screen.exitonclick()
